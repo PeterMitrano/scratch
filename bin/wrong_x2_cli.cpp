6d14f7d5cdc6fa8b562e65c4accfb73ac483f357
Peter Mitrano <mitranopeter@gmail.com>
2018-05-27 13:40:17 -0400

    more example

--- bin/wrong_x2_cli.cpp
+++ bin/wrong_x2_cli.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 #include "core/core.h"
+// but this should NOT work
+// #include "other_dir/bad.h"
 
 int main() {
   std::cout << x2(2) << '\n';
---
+++ other_dir/bad.h
@@ -0,0 +1,1 @@
+#warning "this should not be included"
